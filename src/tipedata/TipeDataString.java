f99bb3fbdf4ceaedf98f059d476d1c2f12f8638c
AdiPranoto <xrayking12@gmail.com>
2023-11-25 20:37:17 +0700

    first Cmmit

---
+++ src/tipedata/TipeDataString.java
@@ -0,0 +1,16 @@
+package tipedata;
+
+public class TipeDataString {
+    public static void main(String[] args) {
+        String firstName = "Adi";
+        String lastName = "Pranoto";
+
+        String fullName = firstName + " " + lastName;
+
+        System.out.println(firstName);
+        System.out.println(lastName);
+
+        System.out.println(fullName);
+
+    }
+}
